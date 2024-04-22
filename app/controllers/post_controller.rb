0c08190fff47e003ccd9bbe5b1dc0bf56b7a5607
Yousef Elsayed <yousef4511@outlook.com>
2024-04-23 01:35:56 +0200

    Preform CRUD operations on Post model

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,27 @@
 class PostController < ApplicationController
   def index
+    # Create & Read
+    post = Post.create({title: "Cool Title", content: "Cool content"})
+    posts = Post.all
+    posts.each do |post|
+      puts "#{post.id} : #{post.title} : #{post.content} : #{post.created_at} : #{post.updated_at}"
+    end
+
+    # Update & Read
+    updated_post = Post.where(id: post.id).update(content: "yousef edited content")
+    posts = Post.all
+    posts.each do |post|
+      puts "#{post.id} : #{post.title} : #{post.content} : #{post.created_at} : #{post.updated_at}"
+    end
+
+    # Delete & Read
+    Post.delete_by(id: updated_post[0].id)
+    posts = Post.all
+    posts.each do |post|
+      puts "#{post.id} : #{post.title} : #{post.content} : #{post.created_at} : #{post.updated_at}"
+    end
+
+    # Delete
+
   end
 end
