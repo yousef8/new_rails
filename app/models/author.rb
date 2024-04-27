2f18d74140db9dc337bc5f55ecc8001eabfe8c74
Yousef Elsayed <yousef4511@outlook.com>
2024-04-27 18:40:55 +0300

    Validate author creation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :dob, presence: true
+  validates :phone_number, presence: true
+  validates :email, format: {with: /\A[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+\z/}
 end
