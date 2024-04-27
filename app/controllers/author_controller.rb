5640addd228ae9bcf1a1c547475909931bffc9d4
Yousef Elsayed <yousef4511@outlook.com>
2024-04-27 18:25:29 +0300

    Handle author creation

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -6,4 +6,21 @@ class AuthorController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new({name: params[:author][:name],
+    email: params[:author][:email],
+    dob: params[:author][:dob],
+    phone_number: params[:author][:phone_number]})
+
+    if @author.save
+      redirect_to authors_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,9 @@ Rails.application.routes.draw do
   delete "posts/:id", to: "post#destroy"
 
   get "authors", to: "author#index", as: "authors"
-  # get "authors/new", to: "author#new", as: "new_author"
+  get "authors/new", to: "author#new", as: "new_author"
   get "authors/:id", to: "author#show", as: "author"
-  # post "authors", to: "author#create"
+  post "authors", to: "author#create"
   # get "authors/:id/edit", to: "author#edit", as: "edit_author"
   # patch "authors/:id", to: "author#update"
   # delete "authors/:id", to: "author#destroy"
