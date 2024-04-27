b74876fac6da4be4102a03f2b17c1aec6ef8099b
Yousef Elsayed <yousef4511@outlook.com>
2024-04-27 15:46:07 +0300

    Create a post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -7,4 +7,16 @@ class PostController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new({title: params[:post][:title], content: params[:post][:content]})
+    if @post.save
+      redirect_to posts_url
+    else
+      render :new, status: 422
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "posts", to: "post#index"
+  get "posts/new", to: "post#new", as: "new_post"
   get "posts/:id", to: "post#show", as: "post"
+  post "posts", to: "post#create"
 end
