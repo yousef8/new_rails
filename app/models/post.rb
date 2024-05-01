4ca3ba9bef84988d0e4c3b5488fd157b1e0c9e40
Yousef Elsayed <yousef4511@outlook.com>
2024-05-01 18:31:11 +0300

    Enhance Creator 1:M Post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author
   belongs_to :creator, class_name: 'Author', foreign_key: 'author_id'
   has_many :post_emojis
   has_many :emojis, through: :post_emojis
