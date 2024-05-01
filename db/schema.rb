4edf9385ec18d2d377c9191812913d5cb3a98f22
Yousef Elsayed <yousef4511@outlook.com>
2024-05-01 18:51:05 +0300

    Create Post with scaffold and api with Author 1:M Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_154508) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_154910) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
 end
